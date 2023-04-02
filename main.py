43837504f496f6a7ea4dcf40a509300ec4d7cd36
Stefan Toma <stefan@Stefans-MBP.mbrv>
2023-04-02 17:22:16 +0300

    min max values - working version

--- main.py
+++ main.py
@@ -26,11 +26,71 @@ def main():
 
         return round(total_valori_vanzare / nr_banci, 2)
 
+    def get_min_max_values(action):
+        banks = table_currency.tbody.select(f"tr > td:nth-of-type(1)")
+        valori_raw = table_currency.tbody.select(f"tr > td:nth-of-type({action})")
+        maximum = float(valori_raw[0].text)
+        minimum = float(valori_raw[0].text)
+        bank_maximum = ""
+        bank_minimum = ""
+
+        for valoare in range(len(valori_raw)):
+            if float(valori_raw[valoare].text) > maximum:
+                maximum = float(valori_raw[valoare].text)
+                bank_maximum = banks[valoare].text
+
+            if float(valori_raw[valoare].text) < minimum:
+                minimum = float(valori_raw[valoare].text)
+                bank_minimum = banks[valoare].text
+
+        return {
+            'max_values':
+            {
+                'bank': bank_maximum,
+                'value': maximum
+            },
+            'min_values':
+            {
+                'bank': bank_minimum,
+                'value': minimum
+            }}
+
+    min_max_vanzare = get_min_max_values(3)
+    min_max_cumparare = get_min_max_values(2)
+
     html_body = f"""
+    
+    <h2> Vanzare: </h2>
+    <h4>&nbsp; &nbsp; Media vanzare: {find_average_value_action(3)} </h4>
+    <h4>&nbsp; &nbsp;
+        Valoarea maxima: {min_max_vanzare['max_values']['value']} 
+        este la: 
+        {min_max_vanzare['max_values']['bank']} 
+    </h4>
+    
+    <h4>&nbsp; &nbsp;
+        Valoarea minima: {min_max_vanzare['min_values']['value']} 
+        este la: 
+        {min_max_vanzare['min_values']['bank']} 
+    </h4>
     <br/>
-    <h3> Media vanzare: {find_average_value_action(3)} </h3>
-    <h3> Media cumparare: {find_average_value_action(2)} </h3>
+    
+    <h2> Cumparare: </h2>
+    <h4>&nbsp; &nbsp; Media cumparare: {find_average_value_action(2)} </h4>
+    <h4>&nbsp; &nbsp;
+        Valoarea maxima: {min_max_cumparare['max_values']['value']} 
+        este la: 
+        {min_max_cumparare['max_values']['bank']} 
+    </h4>
+    <h4>&nbsp; &nbsp; 
+        Valoarea minima: {min_max_cumparare['min_values']['value']} 
+        este la: 
+        {min_max_cumparare['min_values']['bank']} 
+    </h4>
+    
     <br/>
+    <br/>
+    
     <table class="column" style="border-spacing:0;width:100%;max-width:500px;vertical-align:top;display:inline-block;">
     {table_currency.thead}
     {table_currency.tbody}
@@ -65,4 +125,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
